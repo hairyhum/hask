6c780702089bcd4ac20be4fedb1c28a206f6e983
Fedotov Daniil <hairyhum@gmail.com>
2012-08-28 18:20:34 +0400

    commit

--- Image2Buffer.hs
+++ Image2Buffer.hs
@@ -1,13 +1,26 @@
+{-# LANGUAGE RankNTypes,TypeOperators #-}
 module Image2Buffer where
 
-    import Codec.Pictur
+    import Codec.Picture
+    import Codec.Picture.Types
+    import Data.Vector.Storable (map,toList)
+    import Data.List
 
     type Buffer = [[Bool]]
 
-    load :: String -> IO Buffer
-    load file_name = do
-        image <- readPng file_name
+    toBuffer :: LumaPlaneExtractable a => Image a -> Buffer
+    toBuffer img = 
+        let luma = extractLumaPlane img
+            lumaData = imageData luma
+            width = imageWidth luma
+        in chunk  width $ toList $ Data.Vector.Storable.map ((<) 128) lumaData
+    
+    chunk :: Int -> [e] -> [[e]]
+    chunk i ls = 
+        let splitter [] _ n = n
+            splitter l c n  = l `c` splitter (drop i l) c n
+        in Data.List.map (take i) (build (splitter ls))
+    
+    build :: (forall b. (a -> b -> b) -> b -> b) -> [a]
+    build g = g (:) []
 
-
-    toBuffer :: DynamicImage -> Buffer
-    toBuffer ()
--- Main.hs
+++ Main.hs
@@ -1,24 +1,26 @@
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleInstances #-}
 module Main where
 
 import Codec.Picture 
+import System.Environment
 import Codec.Picture.Types 
 
---main = do
---    args <- getArgs
---    case args of
---        [input, out] -> run input out
---        _            -> print "Fuck"
+main = do
+    args <- getArgs
+    case args of
+        [input, out] -> run input out
+        _            -> print "Fuck"
 
---run :: String -> String -> IO ()
---run fileIn fileOut = do
---    image <- readPng fileIn
---    either 
---        putStrLn
---        (withImage (writePng fileOut . extractLumaPlane))
---        image
+run :: String -> String -> IO ()
+run fileIn fileOut = do
+    image <- readPng fileIn
+    either 
+        putStrLn
+        (withImg (print . extractLumaPlane))
+        image
 
-withImg :: (forall a. Pixel a => Image a -> b) -> DynamicImage -> b
+withImg :: (forall a. LumaPlaneExtractable a => Image a -> b) -> DynamicImage -> b
 withImg fun (ImageY8 i) = fun i
 withImg fun (ImageYA8 i) = fun i  
 withImg fun (ImageRGB8 i) = fun i
@@ -26,7 +28,8 @@ withImg fun (ImageRGBA8 i) = fun i
 withImg fun (ImageYCbCr8 i) = fun i
 
 
-
+instance Show (Image Pixel8) where
+    show img = show $ imageData img
 
         
 
